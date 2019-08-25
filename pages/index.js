526f02260300430143f6cc313f701cd4133fe59f
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-08-24 22:50:09 -0300

    Added main component

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import UrlShortnerService from '../services'
+//UrlShortnerService.createShortUrl('www.teste.com')
+export default class Main extends Component{
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    render(){
+        return (
+            <h1>EAE</h1>
+        )
+    }
+}
